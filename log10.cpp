7dd3b2b2aed04dc3666cb300c22e41856df3c6c9
bikerglen <glen@DESKTOP-BEAST.localdomain>
2018-06-24 21:34:37 -0600

    initial checkin

---
+++ log10.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define NORM_STEPS       5
+#define ROUNDS           8
+#define ALPHA_FRAC_BITS 12
+
+double   normf[NORM_STEPS];
+uint32_t normi[NORM_STEPS];
+double   alphaf[ROUNDS];
+uint32_t alphai[ROUNDS];
+
+void BuildTables (void);
+
+void clog10_24b (uint32_t x);
+
+int main (int argc, char *argv[])
+{
+	BuildTables ();
+
+	clog10_24b (0x000000);
+	clog10_24b (0x000001);
+	clog10_24b (0x600000);
+	clog10_24b (0x808891);
+	clog10_24b (0xdebeef);
+	clog10_24b (0xFFFFFF);
+
+	return 0;
+}
+
+
+//---------------------------------------------------------------------------------------------
+// Build Alpha Table
+//
+// Calculates log10 (65536), log10 (256), log10 (16), log10 (4) ...
+// Calculates log10 (3/2), log10 (5/4), log10 (9/8), log10 (17/16) ...
+//
+
+void BuildTables (void)
+{
+	printf ("Building normalization table.\n");
+	for (int step = 0; step < NORM_STEPS; step++) {
+		double power = pow (2, (1 << (4-step)));
+		normf[step] = log10 (power);
+		normi[step] = normf[step] * pow (2, ALPHA_FRAC_BITS);
+		printf ("%2d %12.6f %12.6f %12d\n", step, power, normf[step], normi[step]);
+	}
+
+	printf ("Building alpha table.\n");
+	for (int round = 0; round < ROUNDS; round++) {
+		double fraction = (1 + pow (2, -(1 + round)));
+		alphaf[round] = log10 (fraction);
+		alphai[round] = alphaf[round] * pow (2, ALPHA_FRAC_BITS);
+		printf ("%2d %12.6f %12.6f %12d\n", round, fraction, alphaf[round], alphai[round]);
+	}
+	
+	printf ("\n");
+}
+
+
+//---------------------------------------------------------------------------------------------
+// Calculate log10 (x)
+//
+
+void clog10_24b (uint32_t x)
+{
+	// calculate using built in functions and print result
+	double f = (double)x/(double)16777216;
+	double l = log10 (f);
+	printf ("using built-in fp functions: x: %12.6f, y: %12.6f\n", f, l);
+
+	// initialize result variable
+	double y = 0;
+	int32_t yi = 0;
+
+	// normalize input
+	if ((x & 0xFFFF00) == 0) { x <<= 16; y -= normf[0]; yi -= normi[0]; }
+	if ((x & 0xFF0000) == 0) { x <<=  8; y -= normf[1]; yi -= normi[1]; }
+	if ((x & 0xF00000) == 0) { x <<=  4; y -= normf[2]; yi -= normi[2]; }
+	if ((x & 0xC00000) == 0) { x <<=  2; y -= normf[3]; yi -= normi[3]; }
+	if ((x & 0x100000) == 0) { x <<=  1; y -= normf[4]; yi -= normi[4]; }
+
+	// run rounds
+	for (int round = 0; round < ROUNDS; round++) {
+		uint32_t tmp = x + (x >> (1+round)); 
+		if ((tmp & 0xff000000) == 0) { x = tmp; y -= alphaf[round]; yi -= alphai[round]; }
+	}
+
+	// print result
+	printf ("float cordic results:                            %12.6f\n", y);
+	printf ("int cordic result:                               %12.6f\n", (double)yi/(double)pow (2, ALPHA_FRAC_BITS));
+	printf ("\n");
+}
